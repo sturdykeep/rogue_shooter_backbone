812fd20347d75fe4c8d9bf7c3aba7b83002179a1
DevOwl <christian.muehle@devowl.de>
2023-02-15 20:47:01 +0100

    Switch to BackboneGameWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
-import 'package:flame/game.dart';
 import 'package:flutter/widgets.dart';
-import 'package:rogue_shooter/rogue_shooter_game.dart';
+import 'package:rogue_shooter/rogue_shooter_widget.dart';
 
 void main() async {
   runApp(
-    GameWidget(
-      game: RogueShooterGame(),
-    ),
+    const RogueShooterWidget(),
   );
 }
