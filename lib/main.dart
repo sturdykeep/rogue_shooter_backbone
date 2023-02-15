236c41ca63a9a6e0c38379a44c06b77ccdad031e
DevOwl <christian.muehle@devowl.de>
2023-02-15 20:39:54 +0100

    Clean up

--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,9 @@ import 'package:flutter/widgets.dart';
 import 'package:rogue_shooter/rogue_shooter_game.dart';
 
 void main() async {
-  runApp(GameWidget(game: RogueShooterGame()));
+  runApp(
+    GameWidget(
+      game: RogueShooterGame(),
+    ),
+  );
 }
--- lib/systems/control_system.dart
+++
@@ -1,6 +0,0 @@
-import 'package:backbone/prelude/input/mod.dart';
-import 'package:backbone/realm.dart';
-
-void controlSystem(Realm realm) {
-  final input = realm.getResource<Input>();
-}
