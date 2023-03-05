548b7fac5bb4c4f5287716bf5eb1b5dc70666148
DevOwl <christian.muehle@devowl.de>
2023-03-05 10:30:26 +0100

    Add perfmon for profile mode

--- lib/rogue_shooter_game.dart
+++ lib/rogue_shooter_game.dart
@@ -2,11 +2,14 @@ import 'dart:math';
 
 import 'package:backbone/backbone.dart';
 import 'package:backbone/builders.dart';
+import 'package:backbone/logging/log.dart';
 import 'package:backbone/realm_mixin.dart';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flame/sprite.dart';
+import 'package:flutter/foundation.dart';
+import 'package:perfmon_logger/perfmon_logger.dart';
 import 'package:rogue_shooter/node/player_node.dart';
 
 import 'package:rogue_shooter/node/screen_text_node.dart';
@@ -71,11 +74,15 @@ class RogueShooterGame extends FlameGame
       playerAnimation,
       bullet,
     );
+    Log? logger;
+    if (kProfileMode) {
+      logger = PerfmonLogger();
+    }
     realm = RealmBuilder()
         .withPlugin(defaultPlugin)
         .withPlugin(_roguePlugin)
         .withResource(SpriteSheetStorage, sprietStorage)
-        .build();
+        .build(realmLogger: logger);
     add(realm);
     //Create an initial starfield
     const gapSize = 12;
@@ -97,7 +104,6 @@ class RogueShooterGame extends FlameGame
       ),
       ScreenTextNode(Vector2(15, size.y - 100)),
     ]);
-    realm.logPerformanceData = true;
     realmReady = true;
   }
 
