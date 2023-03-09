b7989d7cf80fa3d3e9d6f4db9848a8e1e565ea34
skyne98 <ahalahan@gmail.com>
2023-03-09 02:45:51 +0100

    Implement collisions using a quad tree

---
+++ lib/quad_tree.dart
@@ -0,0 +1,265 @@
+import 'dart:math';
+
+import 'package:backbone/prelude/transform.dart';
+import 'package:flame/extensions.dart';
+
+class AABB {
+  final Vector2 position;
+  final Vector2 size;
+
+  AABB(this.position, this.size);
+
+  factory AABB.fromTransform(Transform transform) {
+    final topLeft = transform.globalTransformMatrix.transform2(
+      Vector2.zero(),
+    );
+    final topRight = transform.globalTransformMatrix.transform2(
+      Vector2(transform.size.x, 0),
+    );
+    final bottomLeft = transform.globalTransformMatrix.transform2(
+      Vector2(0, transform.size.y),
+    );
+    final bottomRight = transform.globalTransformMatrix.transform2(
+      Vector2(transform.size.x, transform.size.y),
+    );
+
+    // Min and max values
+    final minX = min(
+      min(topLeft.x, topRight.x),
+      min(bottomLeft.x, bottomRight.x),
+    );
+    final maxX = max(
+      max(topLeft.x, topRight.x),
+      max(bottomLeft.x, bottomRight.x),
+    );
+    final minY = min(
+      min(topLeft.y, topRight.y),
+      min(bottomLeft.y, bottomRight.y),
+    );
+    final maxY = max(
+      max(topLeft.y, topRight.y),
+      max(bottomLeft.y, bottomRight.y),
+    );
+
+    // Build AABB
+    final position = Vector2(minX, minY);
+    final size = Vector2(maxX - minX, maxY - minY);
+    return AABB(position, size);
+  }
+
+  bool collidesWith(AABB aabb) {
+    return position.x < aabb.position.x + aabb.size.x &&
+        position.x + size.x > aabb.position.x &&
+        position.y < aabb.position.y + aabb.size.y &&
+        position.y + size.y > aabb.position.y;
+  }
+
+  bool containsPoint(Vector2 point) {
+    return point.x >= position.x &&
+        point.x <= position.x + size.x &&
+        point.y >= position.y &&
+        point.y <= position.y + size.y;
+  }
+
+  bool contains(AABB aabb) {
+    return containsPoint(aabb.position) &&
+        containsPoint(aabb.position + aabb.size);
+  }
+
+  @override
+  String toString() {
+    return 'AABB(position: $position, size: $size)';
+  }
+}
+
+class QuadTree {
+  static const int maxObjects = 10;
+
+  final List<AABB> objects = [];
+  final AABB bounds;
+  final List<QuadTree> nodes = [];
+
+  QuadTree({
+    required this.bounds,
+  });
+
+  void clear() {
+    objects.clear();
+    nodes.forEach((node) => node.clear());
+    nodes.clear();
+  }
+
+  void subdivide() {
+    final subWidth = bounds.size.x / 2;
+    final subHeight = bounds.size.y / 2;
+    final x = bounds.position.x;
+    final y = bounds.position.y;
+
+    nodes.add(
+      QuadTree(
+        bounds: AABB(
+          Vector2(x + subWidth, y),
+          Vector2(subWidth, subHeight),
+        ),
+      ),
+    );
+    nodes.add(
+      QuadTree(
+        bounds: AABB(
+          Vector2(x, y),
+          Vector2(subWidth, subHeight),
+        ),
+      ),
+    );
+    nodes.add(
+      QuadTree(
+        bounds: AABB(
+          Vector2(x, y + subHeight),
+          Vector2(subWidth, subHeight),
+        ),
+      ),
+    );
+    nodes.add(
+      QuadTree(
+        bounds: AABB(
+          Vector2(x + subWidth, y + subHeight),
+          Vector2(subWidth, subHeight),
+        ),
+      ),
+    );
+  }
+
+  bool insert(AABB aabb) {
+    if (bounds.contains(aabb) == false) {
+      return false;
+    }
+
+    if (objects.length < maxObjects && nodes.isEmpty) {
+      objects.add(aabb);
+      return true;
+    }
+
+    // Otherwise, we need to subdivide first
+    if (nodes.isEmpty) {
+      subdivide();
+    }
+
+    // Insert into the appropriate node
+    for (final node in nodes) {
+      if (node.insert(aabb)) {
+        return true;
+      }
+    }
+
+    // Otherwise, the object cannot be inserted for some reason (this should never happen)
+    return false;
+  }
+
+  List<AABB> query(AABB range, {bool contains = false}) {
+    final found = <AABB>[];
+
+    // Automatically abort if the range does not intersect this quad
+    if (bounds.collidesWith(range) == false) {
+      return found;
+    }
+
+    // Check objects at this quad level
+    for (final object in objects) {
+      if (contains && range.contains(object)) {
+        found.add(object);
+      } else if (range.collidesWith(object)) {
+        found.add(object);
+      }
+    }
+
+    // Terminate here, if there are no children
+    if (nodes.isEmpty) {
+      return found;
+    }
+
+    // Otherwise, add the objects from the children
+    for (final node in nodes) {
+      found.addAll(node.query(range));
+    }
+
+    return found;
+  }
+
+  int get totalObjects {
+    var total = objects.length;
+    for (final node in nodes) {
+      total += node.totalObjects;
+    }
+    return total;
+  }
+
+  @override
+  String toString() {
+    // Formatted with indentation
+    const indent = '  ';
+    final sb = StringBuffer();
+    sb.writeln(
+        'QuadTree(bounds: $bounds, objects: ${objects.length}, total: $totalObjects)');
+    for (final node in nodes) {
+      sb.writeln(indent + node.toString().replaceAll('\n', '\n$indent'));
+    }
+    return sb.toString();
+  }
+}
+
+class QuadTreeStack {
+  final AABB bounds;
+  final List<QuadTree> nodes = [];
+
+  QuadTreeStack(this.bounds);
+
+  void clear() {
+    nodes.forEach((node) => node.clear());
+    nodes.clear();
+  }
+
+  void insert(AABB aabb) {
+    if (bounds.contains(aabb) == false) {
+      return;
+    }
+
+    while (true) {
+      // Try to insert into all the existing nodes, until one inserts
+      // or add a new node
+      var inserted = false;
+      for (final node in nodes) {
+        if (node.insert(aabb)) {
+          inserted = true;
+          break;
+        }
+      }
+
+      if (inserted) {
+        break;
+      }
+
+      // Otherwise, we need to subdivide first
+      final node = QuadTree(bounds: bounds);
+      if (node.insert(aabb) == false) {
+        throw StateError('Could not insert into new node');
+      }
+      nodes.add(node);
+    }
+  }
+
+  List<AABB> query(AABB range) {
+    final found = <AABB>[];
+
+    // Automatically abort if the range does not intersect this quad
+    if (bounds.collidesWith(range) == false) {
+      return found;
+    }
+
+    // Check objects at this quad level
+    for (final node in nodes) {
+      found.addAll(node.query(range));
+    }
+
+    return found;
+  }
+}
--- lib/systems/collision_system.dart
+++ lib/systems/collision_system.dart
@@ -1,3 +1,4 @@
+import 'package:backbone/entity.dart';
 import 'package:backbone/filter.dart';
 import 'package:backbone/prelude/transform.dart';
 import 'package:backbone/realm.dart';
@@ -6,59 +7,56 @@ import 'package:flame/extensions.dart';
 import 'package:rogue_shooter/entity/bullet_entity.dart';
 import 'package:rogue_shooter/entity/enemy_entity.dart';
 import 'package:rogue_shooter/entity/player_entity.dart';
+import 'package:rogue_shooter/quad_tree.dart';
 import 'package:rogue_shooter/trait/collision_trait.dart';
-
-class AABB {
-  final Vector2 position;
-  final Vector2 size;
-
-  AABB(this.position, this.size);
-
-  AABB.fromTransform(Transform transform)
-      : position = transform.position,
-        size = transform.size;
-
-  bool collidesWith(AABB other) {
-    final xOverlap =
-        (position.x - other.position.x).abs() * 2 < (size.x + other.size.x);
-    final yOverlap =
-        (position.y - other.position.y).abs() * 2 < (size.y + other.size.y);
-    return xOverlap && yOverlap;
-  }
-
-  bool collidesWithTransform(Transform transform) {
-    return collidesWith(AABB.fromTransform(transform));
-  }
-}
+import 'package:rogue_shooter/trait/to_remove_trait.dart';
 
 void collisionSystem(Realm realm) {
   final query = realm.query(Has([CollisionTrait]));
-  final player = query.whereType<PlayerEntity>().first;
-  final playerTransform = player.get<Transform>();
-  final enemies = query.whereType<EnemyEntity>();
   final bullets = query.whereType<BulletEntity>();
 
+  final player = query.whereType<PlayerEntity>().first;
+
+  // Map entities to AABBs
+  final entityToAABB = <Entity, AABB>{};
+  final AABBToEntity = <AABB, Entity>{};
+  for (final entity in query) {
+    final transform = entity.get<Transform>();
+    final aabb = AABB.fromTransform(transform);
+    entityToAABB[entity] = aabb;
+    AABBToEntity[aabb] = entity;
+  }
+  // Build up a QuadTree
+  final quadTreeStack =
+      QuadTreeStack(AABB(Vector2(-200, -200), realm.gameRef.size * 1.5));
+  for (final aabb in entityToAABB.values) {
+    quadTreeStack.insert(aabb);
+  }
+
   // Player <-> Enemies
-  for (final enemy in enemies) {
-    final enemyTransform = enemy.get<Transform>();
-    if (collision(playerTransform, enemyTransform)) {
-      enemy.takeHit();
+  final playerAABB = entityToAABB[player]!;
+  final playerCollisions = quadTreeStack.query(playerAABB);
+  for (final collision in playerCollisions) {
+    final entity = AABBToEntity[collision]!;
+    if (entity is EnemyEntity) {
+      entity.takeHit();
     }
   }
 
   // Bullets <-> Enemies
+  final toRemove = <Entity>[];
   for (final bullet in bullets) {
-    final bulletTransform = bullet.get<Transform>();
-    for (final enemy in enemies) {
-      final enemyTransform = enemy.get<Transform>();
-      if (collision(bulletTransform, enemyTransform)) {
-        enemy.takeHit();
-        // bullet.destroy();
+    final bulletAABB = entityToAABB[bullet]!;
+    final bulletCollisions = quadTreeStack.query(bulletAABB);
+    for (final collision in bulletCollisions) {
+      final entity = AABBToEntity[collision]!;
+      if (entity is EnemyEntity) {
+        entity.takeHit();
+        toRemove.add(bullet);
       }
     }
   }
-}
 
-bool collision(Transform a, Transform b) {
-  return AABB.fromTransform(a).collidesWithTransform(b);
+  // Remove entities
+  toRemove.forEach((entity) => realm.removeEntity(entity));
 }
